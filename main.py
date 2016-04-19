f026ca661e3e55f9e2e85cca52c08b713b0822ed
softkevin53 <suda1125@outlook.com>
2016-04-19 14:40:10 +0800

    Complete

--- main.py
+++ main.py
@@ -119,4 +119,4 @@ def delete():
 	except:
 		conn.rollback()
 		return "error"
-	return "ok"sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+	return "ok"ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
